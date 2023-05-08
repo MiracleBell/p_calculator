0c226afab766fb04c06622b059a9573c3959a7b7
Ivan Perfiliev <ivan.perfiliev.dev@gmail.com>
2023-05-09 02:00:57 +0600

    Change application package and db connection

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -1,16 +1,15 @@
 package database
 
 import (
-	. "../../config"
 	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
 const m_DRIVER_NAME = "mysql"
 
 func OpenConnection() (*sql.DB, error) {
-	var config = LoadConfigDBFromFile()
-	db, err := sql.Open(m_DRIVER_NAME, ToString(config))
+	db, err := sql.Open(m_DRIVER_NAME, "test:test@/project-calculator")
 	if err != nil {
 		log.Fatal("Error opening database:", err)
 		return nil, err
